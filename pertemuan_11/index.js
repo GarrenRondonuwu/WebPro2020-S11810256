7aac52b2f75d0107dc7d6b8e9a170bfeec8e215e
Garren Axel Arie Rondonuwu <69826193+GarrenRondonuwu@users.noreply.github.com>
2020-09-29 14:00:23 -0800

    5th commit

---
+++ pertemuan_11/index.js
@@ -0,0 +1,29 @@
+//Arrow Fungsion
+
+// 1. fungsi dengan menggunakan parameter
+const operasiPenjumlahan = (bilangan1, bilangan2) =>
+  {
+    const hasil =  bilangan1 + bilangan2;
+    return hasil;
+  };
+  const operasiPengurangan = (bilangan1, bilangan2) =>
+  {
+    const hasil =  bilangan1 - bilangan2;
+    return hasil;
+  };
+  console.log(operasiPenjumlahan(3, 4));
+  console.log(operasiPengurangan(10,5));
+
+// 2. fungsi yang tidak menggunakan parameter
+const namaJenisAnjing = () => 
+  {
+    const anjing = ["Pitbull", "Herder", "Rottweiler"];
+    return anjing[Math.floor(Math.random()*(anjing.length))];
+  }
+const jenisAnjing2 = () => 
+  {
+    const anjing = ["Bulldog", "Retriver", "Siberian"];
+    return anjing[Math.floor(Math.random()*(anjing.length))];
+  }
+  console.log(namaJenisAnjing()); 
+  console.log(jenisAnjing2());
