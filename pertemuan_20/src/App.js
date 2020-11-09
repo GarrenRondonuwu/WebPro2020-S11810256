67e29e923b41732a7f15de86e0763a4759d991bc
Garren Axel Arie Rondonuwu <69826193+GarrenRondonuwu@users.noreply.github.com>
2020-11-09 10:23:02 -0800

    11th commit

---
+++ pertemuan_20/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+import Header from './Components/Header';
+
+import Hello from './Components/Hello';
+
+import Content from './Components/Content';
+
+
+
+function App() {
+
+  return (
+
+    <>
+
+      <Header />
+
+      <Hello />
+
+      <Content />
+
+    </>
+
+  );
+
+};
+
+
+
+export default App;
---
+++ pertemuan_20/src/Utils/waktu.js
@@ -0,0 +1,12 @@
+
+export const Year = () =>{
+
+    return new Date().getFullYear()
+
+  };
+
+export const Day = () =>{
+
+    return new Date().getDate()
+
+  };
