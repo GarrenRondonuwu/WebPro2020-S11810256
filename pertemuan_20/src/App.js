7f92b3a9de296bec11bb4b7a638337a552e95431
Garren Axel Arie Rondonuwu <69826193+GarrenRondonuwu@users.noreply.github.com>
2020-11-12 09:30:11 -0800

    11th, 12th, 13th commit

--- pertemuan_20/src/App.js
+++ pertemuan_20/src/App.js
@@ -4,42 +4,24 @@ import Card from './Components/card';
 
 import {person} from './Utils/person';
 
- 
-
-
+import Counter from "./Components/counter" 
 
 function App() {
-
   return (
-
     <>
-
-      {person.map((item) =>{
-
+      {/*person.map((item) =>{
         return(
-
           <Card
-
             name={item.name}
-
             institution={item.institution}
-
             address={item.address}
-
             phone={item.phone}
-
           />
-
         );
-
-      })}
-
+      })*/}
+      <Counter title = "Counter Component"/>
     </>
-
   );
 
-};
-
-
-
+}
 export default App;
